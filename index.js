a1f44163516868bff154a566c0b15602d4dbf9ef
michelle nderi <nderimichelle@gmail.com>
2023-03-30 01:57:26 +0300

    Comlete assignment

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
-// Add your code here
+// Make a POST request to add data to the db
+
+function submitData(userName,userEmail){
+  return fetch("http://localhost:3000/users", {
+  method: "POST",
+  headers: {
+    "Content-Type": "application/json",
+    Accept: "application/json",
+  },
+  body: JSON.stringify({
+    name:userName ,
+    email:userEmail ,
+  }),
+})
+.then(response =>response.json())
+.then(object => document.body.innerHTML = object.id)
+.catch((error) =>document.body.innerHTML = error.message) 
+}
